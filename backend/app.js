3ca924f95800053fcaf875ee3f22fe4c4d0c44e5
Elokaily <mahmoudelokaily3@gmail.com>
2025-07-20 06:19:02 +0300

    install some packages & connect with database

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,40 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-    res.end("Hello World");
+// ** middlewares
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'Hair dresser',
+        image: 'some_url',
+    }
+    res.send(product);
 })
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    res.send(newProduct);
+})
+//**
+mongoose.connect(process.env.CONNECTION_STRING , {
+    dbName: process.env.DB_NAME,
+})
+    .then(() => {
+        console.log('✅ Connected to MongoDB 🚀');
+    })
+    .catch((err) => {
+        console.error('❌ MongoDB connection error:', err);
+    })
 
 app.listen(3000 , () => {
-    console.log('Server is running at http://localhost:3000');
+    console.log('🌐 Server is running at http://localhost:3000');
 });
